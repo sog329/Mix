58c2fbe3222b7e3e2e2345dd5dd3be0642e65c53
sog329 <songxg0329@163.com>
2023-05-05 00:07:56 +0800

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -107,22 +107,31 @@ class BallTemplate {
   final Color color;
 
   static final Map<int, BallTemplate> map = {
-    0: BallTemplate(0, 2, const Color.fromARGB(255, 232, 211, 192)),
-    1: BallTemplate(1, 2.5, const Color.fromARGB(255, 183, 127, 112)),
-    2: BallTemplate(2, 3, const Color.fromARGB(255, 214, 195, 139)),
-    3: BallTemplate(3, 3.5, const Color.fromARGB(255, 132, 155, 145)),
-    4: BallTemplate(4, 4, const Color.fromARGB(255, 167, 154, 137)),
-    5: BallTemplate(5, 4.5, const Color.fromARGB(255, 194, 206, 220)),
-    6: BallTemplate(6, 5, const Color.fromARGB(255, 176, 177, 182)),
-    7: BallTemplate(7, 5.5, const Color.fromARGB(255, 151, 151, 113)),
-    8: BallTemplate(8, 6, const Color.fromARGB(255, 145, 173, 158)),
-    9: BallTemplate(9, 6.5, const Color.fromARGB(255, 104, 103, 137)),
+    0: BallTemplate(0, 2, Colors.primaries[0]),
+    1: BallTemplate(1, 3, Colors.primaries[2]),
+    2: BallTemplate(2, 4, Colors.primaries[4]),
+    3: BallTemplate(3, 5, Colors.primaries[6]),
+    4: BallTemplate(4, 6, Colors.primaries[8]),
+    5: BallTemplate(5, 7, Colors.primaries[10]),
+    6: BallTemplate(6, 8, Colors.primaries[12]),
+    7: BallTemplate(7, 9, Colors.primaries[14]),
+
+    // 0: BallTemplate(0, 2, const Color.fromARGB(255, 232, 211, 192)),
+    // 1: BallTemplate(1, 2.5, const Color.fromARGB(255, 183, 127, 112)),
+    // 2: BallTemplate(2, 3, const Color.fromARGB(255, 214, 195, 139)),
+    // 3: BallTemplate(3, 3.5, const Color.fromARGB(255, 132, 155, 145)),
+    // 4: BallTemplate(4, 4, const Color.fromARGB(255, 167, 154, 137)),
+    // 5: BallTemplate(5, 4.5, const Color.fromARGB(255, 194, 206, 220)),
+    // 6: BallTemplate(6, 5, const Color.fromARGB(255, 176, 177, 182)),
+    // 7: BallTemplate(7, 5.5, const Color.fromARGB(255, 151, 151, 113)),
+    // 8: BallTemplate(8, 6, const Color.fromARGB(255, 145, 173, 158)),
+    // 9: BallTemplate(9, 6.5, const Color.fromARGB(255, 104, 103, 137)),
   };
 
   BallTemplate(this.id, this.radius, this.color);
 
   static BallTemplate? random() {
-    int tmp = Random().nextInt(BallTemplate.map.length - 1);
+    int tmp = Random().nextInt(BallTemplate.map.length ~/ 2);
     return BallTemplate.map[tmp];
   }
 
@@ -132,7 +141,7 @@ class BallTemplate {
 
   static Color lastColor(BallTemplate t) {
     BallTemplate? tmp = map[t.id - 1];
-    return tmp == null ? Colors.white : tmp!.color;
+    return tmp == null ? Colors.red[200]! : tmp!.color;
   }
 }
 
@@ -169,9 +178,8 @@ class Ball extends BodyComponent with Tappable, Percent {
     super.update(dt);
     if (!end && fromTemplate != null) {
       double p = getPercent(500);
-      paint.color = Color.lerp(fromTemplate!.color, toTemplate.color, p)!;
-      body.fixtures.first.shape.radius =
-          fromTemplate!.radius + (toTemplate.radius - fromTemplate!.radius) * p;
+      _setColor(p);
+      _setRadius(body.fixtures.first.shape, p);
     }
   }
 
@@ -182,13 +190,21 @@ class Ball extends BodyComponent with Tappable, Percent {
     required this.toTemplate,
   });
 
+  void _setColor(double p) {
+    Color from = BallTemplate.lastColor(toTemplate);
+    Color c = Color.lerp(from, toTemplate.color, p)!;
+    paint.shader = LinearGradient(colors: [from, c]).createShader(
+        Rect.fromCircle(center: Offset.zero, radius: toTemplate.radius));
+  }
+
+  void _setRadius(Shape s, double p) {
+    s.radius =
+        fromTemplate!.radius + (toTemplate.radius - fromTemplate!.radius) * p;
+  }
+
   @override
   Body createBody() {
-    paint.color = toTemplate.color;
-    paint.shader = LinearGradient(
-            colors: [BallTemplate.lastColor(toTemplate), toTemplate.color])
-        .createShader(
-            Rect.fromCircle(center: Offset.zero, radius: toTemplate.radius));
+    _setColor(1);
     final shape = CircleShape();
     shape.radius = toTemplate.radius;
 
